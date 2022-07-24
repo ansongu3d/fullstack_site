f75341056d20e21c16ae3e2083290a730e6e2292
ag3d <sydneybettafish@gmail.com>
2022-07-24 11:05:55 +1000

    commit 3rd

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
+import os
+import psycopg2
 from flask import Flask
 
-app = Flask(__name__)
+
+# get DATABASE_URL value from environment,
+# if the value is NOT found, then use 'dbname=food_truck'
+DATABASE_URL = os.environ.get('HEROKU_POSTGRESQL_CYAN_URL', 'dbname=food_truck')
+
+# get SECRET_KEY value from environment,
+# if the value is NOT found, then use 'bambi-thumper-example'
+#
+# Tip:
+# 
+# You can use the following code snippet to generate random secret key for your production environment
+#   import secrets
+#   secrets.token_hex(16)
+SECRET_KEY = os.environ.get('SECRET_KEY', 'bambi-thumper-example')
+
+app = Flask (__name__)
+app.secret_key = SECRET_KEY.encode()
 
 @app.route('/')
 def index():
-    return '<h1>Hello world! AG!!!</h1>'
+    connection = psycopg2.connect(DATABASE_URL)
+    cursor = connection.cursor()
+    cursor.execute("SELECT 1")
+    connection.close()
+    return '<h1>hello, world</h1>'
 
 if __name__ == "__main__":
     app.run(debug=True)
