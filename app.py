ba2bd6d691b0d2572603e3bea7b77d2c0029d543
ag3d <sydneybettafish@gmail.com>
2022-07-27 20:59:32 +1000

    add collection page

--- app.py
+++ app.py
@@ -23,36 +23,41 @@ app.secret_key = SECRET_KEY.encode()
 
 @app.route('/')
 def index():
-    # connection = psycopg2.connect(DATABASE_URL)
-    # cursor = connection.cursor()
-    # cursor.execute("""
-    #     SELECT id, name, image_url1, image_url2, price
-    #     FROM toy
-    #     LIMIT 100
-    # """)
-    # result = cursor.fetchall()
-    
-    # toy_collection = result
-
-    # # using the cookie given to us by the browser
-    # # look up username in our DB
-    # user_id_from_encrypted_cookie = session.get("user_id")
-    # if user_id_from_encrypted_cookie:
-    #     cursor.execute("""
-    #         SELECT username
-    #         FROM users
-    #         WHERE id = %s
-    #         LIMIT 1
-    #     """, (user_id_from_encrypted_cookie,))
-    #     result = cursor.fetchone()
-    # else:
-    #     result = None
-    # if result:
-    #     username = result
-    # else:
-    #     username = None
-
     return render_template("index.html")
+
+
+@app.route('/collection')
+def collection():
+    connection = psycopg2.connect(DATABASE_URL)
+    cursor = connection.cursor()
+    cursor.execute("""
+        SELECT id, name, image_url_1, image_url_2, qty, price
+        FROM toy
+        LIMIT 100
+    """)
+    result = cursor.fetchall()
+    
+    toy_collection = result
+
+    # using the cookie given to us by the browser
+    # look up username in our DB
+    user_id_from_encrypted_cookie = session.get("user_id")
+    if user_id_from_encrypted_cookie:
+        cursor.execute("""
+            SELECT username
+            FROM users
+            WHERE id = %s
+            LIMIT 1
+        """, (user_id_from_encrypted_cookie,))
+        result = cursor.fetchone()
+    else:
+        result = None
+    if result:
+        username = result
+    else:
+        username = None
+
+    return render_template("collection.html", toy=toy_collection, username=username)
     
 
 if __name__ == "__main__":
